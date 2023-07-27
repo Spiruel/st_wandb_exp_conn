f70051f3db0739bd6847ef41a67a6ff8c090fabc
Samuel Bancroft <spiruel@gmail.com>
2023-07-27 01:10:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,26 @@ import streamlit as st
 from st_files_connection import FilesConnection
 import numpy as np
 
+class WandbConnector:
+
+    def __init__(self):
+        self._conn = None
+
+    def _connect(self):
+        self._conn = st.experimental_connection('wandb', type=FilesConnection)
+
+    def cursor(self):
+        if not self._conn:
+            self._connect()
+        return self._conn
+
+    def query(self, path):
+        return self.cursor().read(path, input_format='text')
+
+@st.cache_data  
+def cached_query(connector, path):
+    return connector.query(path)
+    
 st.title('st.experimental_connection with Weights & Biases')
 st.info('''This Streamlit app demonstrates how to access and display data from Weights & Biases machine learning experiments using the new experimental_connection functionality. 
 
@@ -9,7 +29,7 @@ The app establishes a connection to W&B cloud storage using the FilesConnection 
 
 This app is just a basic demo for now, but can be used to create custom interfaces to your model runs.''')
 
-conn = st.experimental_connection('wandb', type=FilesConnection)
+wandb = WandbConnector() 
 
 entity = st.text_input('Wandb Entity eg. `wandb`, `sciml-leeds`', value='wandb')
 
@@ -18,7 +38,7 @@ a = col1.empty()
 b = col2.empty()
 
 try:
-    projects = list(set(conn.fs.ls(entity)))
+    projects = list(set(wandb.cursor().fs.ls(entity)))
 except Exception as e:
     st.error(f'Could not find projects under {entity}')
 else:    
@@ -37,7 +57,7 @@ else:
         st.error(f'Could not find projects under {entity}')
     else:
         
-        runs = conn.fs.ls(f'{entity}/{project}')
+        runs = wandb.cursor().fs.ls(f'{entity}/{project}')
         if len(runs) == 0:
             st.warning('No runs found')
         else:
@@ -53,20 +73,21 @@ else:
                 
             run_id = b.selectbox('Select run', runs, index=run_index)
         
-            files = conn.fs.ls(f'{entity}/{project}/{run_id}')
+            files = wandb.cursor().fs.ls(f'{entity}/{project}/{run_id}')
         
             graph_check = ['graph' in f for f in files]
             if any(graph_check):
                 with st.spinner('loading...'):
                     st.markdown('#### graph')
-                    st.graphviz_chart(conn.read(f'{entity}/{project}/{run_id}/graph', input_format='text'), use_container_width=True)
+                    graph = cached_query(wandb, f'{entity}/{project}/{run_id}/graph')
+                    st.graphviz_chart(graph, use_container_width=True)
                
             media_check = ['media' in f for f in files]
             if any(media_check):
                 with st.spinner('loading images...'):
                     media_src = np.array(files)[media_check]
                     for m_s in media_src:
-                        images = conn.fs.ls(m_s+'/images')
+                        images = wandb.cursor().fs.ls(m_s+'/images')
                         st.markdown('#### images')
                         if len(images) == 0:
                             st.warning('no images')
@@ -77,7 +98,7 @@ else:
                         num_row = 7
                         rows = n//num_row
         
-                        images_conn = [conn.open(img) for img in images]
+                        images_conn = [wandb.cursor().open(img) for img in images]
         
                         for row_idx in range(0, rows, 1):
                             st.image(images_conn[0+row_idx*num_row:0+row_idx*num_row+num_row], width=100)
@@ -89,6 +110,7 @@ else:
             reqs_check = ['requirements.txt' in f for f in files]
             if any(reqs_check):
                 with st.expander('run requirements:'):
-                    st.code(conn.read(f'{entity}/{project}/{run_id}/requirements.txt'))
+                    reqs = cached_query(wandb, f'{entity}/{project}/{run_id}/requirements.txt')
+                    st.code(reqs)
 
 
